cb3794f13bd819212544ddb4c66dfb4ff83f3b96
Arpan Bhattacharya <arpan@Arpans-MBP-10.home>
2020-11-15 15:37:06 -0600

    authentication

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,47 @@ import {
 import NewInitScript from "./NewInitScript";
 import logo from "./logo.svg";
 import "./App.css";
+import {Auth0Provider} from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";
 
 function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<div className="App">
-						<header className="App-header">
-							<img src={logo} className="App-logo" alt="logo" />
-							<p>
-								<b>
+		<Auth0Provider domain = "auth.readily.dev" clientId="zqagGnqCXg8ywh7x4OCRDPxLd9KfYL51" redirectUri={window.location.origin}>
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<div className="App">
+							<header className="App-header">
+								<img src={logo} className="App-logo" alt="logo" />
+								<p>
+									<b>
                   Highly Scalable Developer Infra For Fast-Paced Teams
-								</b>
-							</p>
-							<p>
-								<a
-									className="App-link"
-									href="https://reactjs.org"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
+									</b>
+								</p>
+								<p>
+									<a
+										className="App-link"
+										href="https://reactjs.org"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
                   Create a blueprint
-								</a>
-								{" "}
-								{" "}
+									</a>
+									{" "}
+									{" "}
                 to get dev-servers on-demand!
-							</p>
-						</header>
-					</div>
-				</Route>
-				<Route exact path = "/init">
-					<NewInitScript/>
-				</Route>
-			</Switch>
-		</Router>
+								</p>
+								<LoginButton/>
+							</header>
+
+						</div>
+					</Route>
+					<Route exact path = "/init">
+						<NewInitScript/>
+					</Route>
+				</Switch>
+			</Router>
+		</Auth0Provider>
 	);
 }
 
