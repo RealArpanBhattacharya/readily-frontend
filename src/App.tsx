5fc103ee08e2d54036b5cca383297a0076785416
Arpan Bhattacharya <arpan@Arpans-MBP-10.home>
2020-11-15 00:11:48 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+	BrowserRouter as Router, Route, Switch,
+} from "react-router-dom";
+import NewInitScript from "./NewInitScript";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<div className="App">
+						<header className="App-header">
+							<img src={logo} className="App-logo" alt="logo" />
+							<p>
+								<b>
+                  Highly Scalable Developer Infra For Fast-Paced Teams
+								</b>
+							</p>
+							<p>
+								<a
+									className="App-link"
+									href="https://reactjs.org"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+                  Create a blueprint
+								</a>
+								{" "}
+								{" "}
+                to get dev-servers on-demand!
+							</p>
+						</header>
+					</div>
+				</Route>
+				<Route exact path = "/init">
+					<NewInitScript/>
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/NewInitScript.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function NewInitScript() {
+	return (
+		<>
+			<iframe frameBorder="0" width="100%" height="500px"
+				src="https://repl.it/@RealArpan/ScalyCornsilkLicensing?lite=true"></iframe></>
+	);
+}
+
+export default NewInitScript;
